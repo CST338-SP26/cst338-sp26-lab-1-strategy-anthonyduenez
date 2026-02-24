63bd7419847d6b373230f03a1fa804f30e31d2e7
Anthony Duenez Ramirez <duenezanthony67@gmail.com>
2026-02-24 15:44:29 -0800

    Imp and Kobold classes

---
+++ src/main/java/Kobold.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Kobold extends Monster {
+    public Kobold(Integer maxHp, Integer xp, HashMap<String, Integer> items) {
+        super(maxHp, xp, items);
+    }
+
+    @Override
+    public String toString() {
+        return "Kobold has : " +
+                super.toString();
+    }
+}
